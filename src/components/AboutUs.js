fa2330685d0caa831e401312398787c0c966a0df
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-07-04 00:40:50 -0500

    aligned aboutUs header/content to right

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,13 +3,24 @@ import classes from './AboutUs.module.css';
 
 const aboutUs = props => (
   <div>
-    <div className={classes.Header}>
-     About us
-    </div>
-    <div  className={classes.Content}>
-     We choose the name Vydr.io because glass (vidrio) is the transparent barrier between us and our devices. We want this transparency ot transcend in everything that we do. When it comes to our clients Vydrio seeks to provide clear solutions in an unclear market.
+  
+  <div className={classes.Prime}>
+  <div className={classes.Card2}>
+    <div className={classes.CardInside}>
+      
+      <h2>Image</h2>
+      
     </div>
   </div>
+  <div className={classes.Card}>
+    <div className={classes.CardInside}>
+      
+      <h1>About us</h1>
+      <p>We choose the name Vydr.io because glass (vidrio) is the transparent barrier between us and our devices. We want this transparency ot transcend in everything that we do. When it comes to our clients Vydrio seeks to provide clear solutions in an unclear market.</p>
+    </div>
+  </div>
+</div>
+</div>
   );
   
   export default aboutUs;
