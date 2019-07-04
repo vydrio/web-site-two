6596f3e69ca00c85eb492997d5f43f68cfce1fde
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-07-04 15:48:39 -0500

    aligned content text on aboutUs component

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -7,8 +7,9 @@ const aboutUs = props => (
   <div className={classes.Prime}>
   <div className={classes.Card}>
     <div className={classes.CardInside}>
-      
+      <div className={classes.Header}>
       <h1>About us</h1>
+      </div>
       <p>We choose the name Vydr.io because glass (vidrio) is the transparent barrier between us and our devices. We want this transparency ot transcend in everything that we do. When it comes to our clients Vydrio seeks to provide clear solutions in an unclear market.</p>
     </div>
   </div>
