00b31221299fa3296b13734c1ee25648f3ac637d
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-07-03 23:53:47 -0500

    added component's header/content

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import classes from './AboutUs.module.css';
 
 const aboutUs = props => (
-    <div>
-     About us 
+  <div>
+    <div className={classes.Header}>
+     About us
     </div>
+    <div  className={classes.Content}>
+     We choose the name Vydr.io because glass (vidrio) is the transparent barrier between us and our devices. We want this transparency ot transcend in everything that we do. When it comes to our clients Vydrio seeks to provide clear solutions in an unclear market.
+    </div>
+  </div>
   );
   
   export default aboutUs;
