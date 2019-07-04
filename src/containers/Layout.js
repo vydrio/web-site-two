014514057219350399e4cb2edab36840ec480f8c
Carlosf96 <carlos.f.roque96@gmail.com>
2019-07-04 11:55:09 -0500

    Contact us

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Contact from '../components/Contact'
 
 class Layout extends Component{
   render(){
@@ -8,7 +9,7 @@ class Layout extends Component{
           <p style={{backgroundColor: '#75a9f9'}}>NavBar</p>
           <p style={{backgroundColor: '#ebebeb'}}>About us</p>
           <p style={{backgroundColor: '#f7f6f6'}}>Services</p>
-          <p style={{backgroundColor: '#ffffff'}}>Contact</p>
+          <Contact/>
         </main>
         <footer style={{backgroundColor: '#f7f6f6'}}>Footer</footer>
       </div>
