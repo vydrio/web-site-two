d6cf45694679f1db371ec4e8ce6fa4a4579838fd
IvanCarrilloSolache <carrillo.solache@gmail.com>
2019-07-04 21:41:14 -0500

    content/image is responsive, still alignment for mobile

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -3,24 +3,20 @@ import classes from './AboutUs.module.css';
 
 const aboutUs = props => (
   <div>
-  
-  <div className={classes.Prime}>
-  <div className={classes.Card}>
-    <div className={classes.CardInside}>
-      <div className={classes.Header}>
-      <h1>About us</h1>
+    <div className={classes.Main2}>
+      <div className={classes.Card1}>
+        <div className={classes.CardContent}>
+          <h1 className={classes.Title}>About us</h1>
+          <p>We choose the name Vydr.io because glass (vidrio) is the transparent barrier between us and our devices. We want this transparency ot transcend in everything that we do. When it comes to our clients Vydrio seeks to provide clear solutions in an unclear market.</p>
+        </div>
       </div>
-      <p>We choose the name Vydr.io because glass (vidrio) is the transparent barrier between us and our devices. We want this transparency ot transcend in everything that we do. When it comes to our clients Vydrio seeks to provide clear solutions in an unclear market.</p>
+      <div className={classes.Card2}>
+        <div className={classes.CardContent2}>
+          <img src={require('../assets/logo.jpg')} />
+        </div>
+      </div>
     </div>
   </div>
-  <div className={classes.Card2}>
-    <div className={classes.CardInside1}>
-      <img src={require('../assets/logo.jpg')} />
-    </div>
-  </div>
-  
-</div>
-</div>
-  );
+);
   
   export default aboutUs;
