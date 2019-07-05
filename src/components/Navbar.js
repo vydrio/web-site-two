ee21ccbc76f81ee71ae3d2e0356358c6d3856a48
gersonbarajas62 <gersonbarajas62@gmail.com>
2019-07-05 11:05:27 -0500

    added hamhuger style but still working in position

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './Navbar.css'
+import $ from 'jquery';
 
 
-
+/* only execute this script when the document is ready */
+$(document).ready(function(){
+    /* function called when you click of the button */
+    $("button").click(function(){
+        
+        /* this if else to change the text in the button */
+        if($("button").text() == "☰"){
+         $("button").text("🞬");
+        }else{
+          $("button").text("☰");
+        }
+      
+      /* this function toggle the visibility of our "li" elements */
+      $("li").toggle("slow");
+    });
+  });
 class Navbar extends React.Component{
     render() {
         return (
